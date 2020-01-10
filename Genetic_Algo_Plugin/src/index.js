4b8e9c72c857038d304d2de9e6fcfba91a640344
Nadav Dor <nadavdor274@gmail.com>
2020-01-10 16:45:16 +0200

    adding c major scal to index

--- Genetic_Algo_Plugin/src/index.js
+++ Genetic_Algo_Plugin/src/index.js
@@ -1,1 +1,6 @@
 import { initialize } from '@muzilator/sdk';
+
+// hard coding  major scale.
+// will be changed to input scale in the future.
+var NOTES_COLLECTION = [60, 62, 64, 65, 67, 69, 71, 72];
+
